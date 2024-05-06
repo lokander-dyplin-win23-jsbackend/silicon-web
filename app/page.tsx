e6b333d35b1c0956f8c8637e714126d363a08a41
Markus Lokander <Markuslokander97@gmail.com>
2024-05-06 10:32:29 +0200

    bootstrap/fontawesome and icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://kit.fontawesome.com/81af572429.css"
+          crossOrigin="anonymous"
+        />
+      </head>
+    </main>
+  );
 }

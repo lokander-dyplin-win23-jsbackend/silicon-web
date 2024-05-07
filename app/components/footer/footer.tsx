96de0aebf5fb1b0f817550dcb6f3c8b5727b34f3
Markus Lokander <Markuslokander97@gmail.com>
2024-05-07 09:43:25 +0200

    format and implemented footer/header

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,17 +1,26 @@
 export default function Footer() {
-    return (
-        <footer>
-        <div className="container">
-            <div className="social-media">
-                <a className="btn-social" href="#"><i className="fa-brands fa-facebook"></i></a>
-                <a className="btn-social" href="#"><i className="fa-brands fa-square-x-twitter"></i></a>
-                <a className="btn-social" href="#"><i className="fa-brands fa-instagram"></i></a>
-                <a className="btn-social" href="#"><i className="fa-brands fa-youtube"></i></a>
-            </div>
-            <p>
-                &copy; 2024 Silicon, All rights reserved. Pulvinar urna condimentum amet tempor, ornare integer. Lorem
-                bla bla bla
-            </p>
+  return (
+    <footer>
+      <div className="container">
+        <div className="social-media">
+          <a className="btn-social" href="#">
+            <i className="fa-brands fa-facebook"></i>
+          </a>
+          <a className="btn-social" href="#">
+            <i className="fa-brands fa-square-x-twitter"></i>
+          </a>
+          <a className="btn-social" href="#">
+            <i className="fa-brands fa-instagram"></i>
+          </a>
+          <a className="btn-social" href="#">
+            <i className="fa-brands fa-youtube"></i>
+          </a>
         </div>
+        <p>
+          &copy; 2024 Silicon, All rights reserved. Pulvinar urna condimentum
+          amet tempor, ornare integer. Lorem bla bla bla
+        </p>
+      </div>
     </footer>
-    );}
+  );
+}

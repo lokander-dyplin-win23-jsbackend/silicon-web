a705d00fc3eb3eda3c8f3f06e6173fb36e019f39
Markus Lokander <Markuslokander97@gmail.com>
2024-05-08 08:57:29 +0200

    changed html structure and added css

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -6,26 +6,28 @@ export default function SignIn() {
           <div className="statusMessage"></div>
           <h1> Welcome back</h1>
           <p>
-            Dont have an account?{" "}
-            <a asp-controller="Auth" asp-action="SignUp">
-              Sign up here
-            </a>
-            .
+            Dont have an account? <a href="signup">Sign up here</a>.
           </p>
           <div className="content">
             <div id="form-email" className="input-group">
-              <label asp-for="Email"></label>
+              <label asp-for="Email">Email</label>
               <input asp-for="Email" />
             </div>
             <div id="form-password" className="input-group">
-              <label asp-for="Password"></label>
+              <label asp-for="Password">Password</label>
               <input asp-for="Password" />
             </div>
 
             <div id="form-remember" className="checkbox-group">
               <div className="input-group">
-                <input asp-for="IsPresistent" />
-                <label asp-for="IsPresistent"></label>
+                <input
+                  type="checkbox"
+                  value=""
+                  id="remeberme"
+                  name="remeberme"
+                  // checked={item.checked}
+                />
+                <label>Remeber me</label>
               </div>
             </div>
             <button id="form-submit" className="btn-theme-s" type="submit">
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,62 +1,71 @@
-<section id="signup">
-  <div className="container">
-    <form method="post" asp-controller="Auth" asp-action="SignUp" noValidate>
-      <h1> Create Account</h1>
-      <p>
-        Already have an account?{" "}
-        <a asp-controller="Auth" asp-action="SignIn">
-          Sign in here
-        </a>
-        .
-      </p>
-      <div className="content">
-        <div id="form-firstname" className="input-group">
-          <label asp-for="FirstName"></label>
-          <span asp-validation-for="FirstName"></span>
-          <input asp-for="FirstName" />
-        </div>
-        <div id="form-lastname" className="input-group">
-          <label asp-for="LastName"></label>
-          <span asp-validation-for="LastName"></span>
-          <input asp-for="LastName" />
-        </div>
-        <div id="form-email" className="input-group">
-          <label asp-for="Email"></label>
-          <span asp-validation-for="Email"></span>
-          <input asp-for="Email" />
-        </div>
-        <div id="form-password" className="input-group">
-          <label asp-for="Password"></label>
-          <span asp-validation-for="Password"></span>
-          <input asp-for="Password" />
-        </div>
-        <div id="form-confirm" className="input-group">
-          <label asp-for="ConfirmPassword"></label>
-          <span asp-validation-for="ConfirmPassword"></span>
-          <input asp-for="ConfirmPassword" />
-        </div>
-        <div id="form-terms" className="checkbox-group">
-          <div className="input-group">
-            <input asp-for="TermsAndConditions" />
-            <label asp-for="TermsAndConditions">
-              {" "}
-              I agree to the{" "}
-              <a asp-controller="" asp-action="">
-                {" "}
-                Terms
-              </a>{" "}
-              &{" "}
-              <a asp-controller="" asp-action="">
-                Conditions
-              </a>
-            </label>
+import styles from "./signup.module.css";
+
+export default function SignUp() {
+  return (
+    <section className={styles.signup}>
+      <div className={styles.container}>
+        <form className={styles.form} method="post" noValidate>
+          <h1> Create Account</h1>
+          <p>
+            Already have an account? <a href="/signin">Sign in here</a>.
+          </p>
+          <div className={styles.content}>
+            <div id="form-firstname" className={styles.inputGroup}>
+              <label htmlFor="FirstName">Firstname</label>
+              <span className={styles.validationError}></span>
+              <input type="text" id="FirstName" name="FirstName" />
+            </div>
+            <div id="form-lastname" className={styles.inputGroup}>
+              <label htmlFor="LastName">Lastname</label>
+              <span className={styles.validationError}></span>
+              <input type="text" id="LastName" name="LastName" />
+            </div>
+            <div id="form-email" className={styles.inputGroup}>
+              <label htmlFor="Email">Email</label>
+              <span className={styles.validationError}></span>
+              <input type="email" id="Email" name="Email" />
+            </div>
+            <div id="form-password" className={styles.inputGroup}>
+              <label htmlFor="Password">Password</label>
+              <span className={styles.validationError}></span>
+              <input type="password" id="Password" name="Password" />
+            </div>
+            <div id="form-confirm" className={styles.inputGroup}>
+              <label htmlFor="ConfirmPassword">Confirm Password</label>
+              <span className={styles.validationError}></span>
+              <input
+                type="password"
+                id="ConfirmPassword"
+                name="ConfirmPassword"
+              />
+            </div>
+            <div id="form-terms" className={styles.checkboxGroup}>
+              <div className={styles.inputGroup}>
+                <input
+                  type="checkbox"
+                  value=""
+                  id="agreeterms"
+                  name="agreeterms"
+                  // checked={item.checked}
+                />
+                <label htmlFor="TermsAndConditions">
+                  {" "}
+                  I agree to the <a href="#"> Terms </a> &{" "}
+                  <a href="#"> Conditions </a>
+                </label>
+              </div>
+              <span className={styles.validationError}></span>
+            </div>
           </div>
-          <span asp-validation-for="TermsAndConditions"></span>
-        </div>
+          <button
+            id="form-submit"
+            className={`${styles.btnThemeS} ${styles.formSubmit}`}
+            type="submit"
+          >
+            Sign up
+          </button>
+        </form>
       </div>
-      <button id="form-submit" className="btn-theme-s" type="submit">
-        Sign up
-      </button>
-    </form>
-  </div>
-</section>;
+    </section>
+  );
+}

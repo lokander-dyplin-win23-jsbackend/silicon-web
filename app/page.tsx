8ad3f36d3bd99904ccfda755204aab7e6ec7dedc
Alexander <alexander.dyplin@utb.ecutbildning.se>
2024-05-09 17:17:27 +0200

    homepage
    moved homepage code to global page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
-import Homepage from "./homepage/page";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
-        <Homepage />
-    </main>
+    <section className={styles.aboutUs}>
+            <img className={styles.backgroundImage} src="images/SiliconWeb-Home-Image.svg" alt="" />
+            <div className={`container ${styles.container}`}>
+                    <h1>Who are we?</h1>
+                <div className={styles.content}>
+                    <div className={styles.info}>
+                        <img className={styles.image} src="images/alexD.svg" alt="Alexander" />
+                        <h5>Alexander Dyplin</h5>
+                        <p>alexander.dyplin@domain.com</p>
+                    </div>
+                    <div className={styles.info}>
+                        <img className={styles.image} src="images/markusL.svg" alt="Markus" />
+                        <h5>Markus Lokander</h5>
+                        <p>markus.lokander@domain.com</p>
+                    </div>
+                </div>
+            </div>
+        </section>
   );
 }
